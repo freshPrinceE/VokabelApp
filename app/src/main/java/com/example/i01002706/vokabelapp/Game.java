9ec108161e888ddf2ea6665309aefc5a9ddf88be
erdogan.ekrem@gmx.de <sivaslieko58>
2019-02-26 11:22:53 +0100

    Anpassungen im Spiel, kann gespielt werden

--- app/src/main/java/com/example/i01002706/vokabelapp/Game.java
+++ app/src/main/java/com/example/i01002706/vokabelapp/Game.java
@@ -3,6 +3,8 @@ package com.example.i01002706.vokabelapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -15,7 +17,12 @@ public class Game extends AppCompatActivity {
     private int cardsetId;
     private List<Card> cards = new ArrayList<>();
     private CardDao cardDao;
-    private int count = 0;
+    private static int count = 0;
+    private TextView answer;
+    private Button gewusst;
+    private Button nichtGewusst;
+    private TextView question;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +35,53 @@ public class Game extends AppCompatActivity {
         AppDatabase database = AppDatabase.getDatabase(this);
         cardDao = database.cardDao();
         cards = cardDao.allCards(cardsetId);
-        TextView question = findViewById(R.id.gameQuestion);
-        TextView answer = findViewById(R.id.gameQuestion);
-        Log.d("Test:", String.valueOf(cards.size()));
+        question = findViewById(R.id.gameQuestion);
+        answer = findViewById(R.id.gameAnswer);
+        answer.setText("?");
         question.setText(cards.get(count).getFrage());
-        answer.setText(cards.get(count).getAntwort());
+        gewusst = findViewById(R.id.gewusst);
+        nichtGewusst = findViewById(R.id.nichtGewusst);
+        gewusst.setVisibility(View.INVISIBLE);
+        nichtGewusst.setVisibility(View.INVISIBLE);
+        gewusst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Eintrag in der Tabelle
+                loadData();
+            }
+        });
+        nichtGewusst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Eintrag in der Tabelle
+                loadData();
+            }
+        });
+        View view = findViewById(R.id.touch);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setText(cards.get(count).getAntwort());
+                gewusst.setVisibility(View.VISIBLE);
+                nichtGewusst.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+    }
+    private void loadData(){
+        if(count>=cards.size()-1){
+            count = 0;
+        }else {
+            count++;
+        }
+
+
+        answer.setText("?");
+        question.setText(cards.get(count).getFrage());
+        gewusst = findViewById(R.id.gewusst);
+        nichtGewusst = findViewById(R.id.nichtGewusst);
+        gewusst.setVisibility(View.INVISIBLE);
+        nichtGewusst.setVisibility(View.INVISIBLE);
     }
 }
